16886e8f7c4d7faa6a458fdc028a257625433bb2
lucaMastro <luca.mastrobattista@students.uniroma2.eu>
2022-05-03 20:15:30 +0200

    used inner classes to make each test indipendent from others

--- src/test/java/param_tests/SerializeWriterTest.java
+++ src/test/java/param_tests/SerializeWriterTest.java
@@ -5,95 +5,206 @@ import junit.framework.TestCase;
 
 import com.alibaba.fastjson.serializer.SerializeWriter;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
+@RunWith(Enclosed.class)
 public class SerializeWriterTest extends TestCase {
-	/* test 0 params */
-	private char[] chars_to_append;
-	private Integer[] ints_to_append;
-	private Long[] longs_to_append;
-	private String[] first_output_strings;
-	private String[] second_output_strings;
 
-	private SerializeWriter writer;
-	private final int max_test_index = 6;
-	
+	@RunWith(Parameterized.class)
+	public static class Test_0 {
+		private char aChar;
+		private int anInt;
+		private long aLong;
+		private String first_out;
+		private String second_out;
 
-	public SerializeWriterTest() throws IOException {
-		int len = this.max_test_index + 1;
-		this.chars_to_append = new char[len];
-		this.ints_to_append = new Integer[len];
-		this.longs_to_append = new Long[len];
-		this.first_output_strings = new String[len];
-		this.second_output_strings = new String[len];
-		this.configure();
-	}
+		private SerializeWriter writer;
 
-	public void configure() throws IOException {
-		for (int i = 0; i <= this.max_test_index; i++) {
-			try{
-				this.chars_to_append[i] = Support.getProperty("SerializeWriterTest_test_" + i + "_char").toCharArray()[0];
-			}catch (Exception e){}
-			try{
-				this.ints_to_append[i] = Integer.valueOf(Support.getProperty("SerializeWriterTest_test_" + i + "_int"));
-			}catch (Exception e){}
-			try{
-				String str = Support.getProperty("SerializeWriterTest_test_" + i + "_long");
-				this.longs_to_append[i] = Long.valueOf(Support.stripTrailingL(str));
-			}catch (Exception e){}
-			try{
-				this.first_output_strings[i] = Support.getProperty("SerializeWriterTest_test_" + i + "_first_output");
-			}catch (Exception e){}
-			try{
-				this.second_output_strings[i] = Support.getProperty("SerializeWriterTest_test_" + i + "_second_output");
-			}catch (Exception e){}
+		@Parameterized.Parameters
+		public static Collection<Object[]> data() {
+			return Arrays.asList(new Object[][] {
+					{ 'A', 156, 345 }
+			});
 		}
-		this.writer = new SerializeWriter();
+
+		public Test_0(char aChar, int anInt, long aLong){
+			this.writer = new SerializeWriter();
+			this.configure(aChar, anInt, aLong);
+		}
+
+		public void configure(char aChar, int anInt, long aLong){
+			this.aChar = aChar;
+			this. anInt = anInt;
+			this.aLong = aLong;
+
+			StringBuilder outputs = new StringBuilder();
+			outputs.append(aChar).append(anInt);
+			this.first_out = outputs.toString();
+			outputs.append(aLong);
+			this.second_out = outputs.toString();
+		}
+
+		@Test
+		public void test_0() throws Exception {
+			//SerializeWriter writer = new SerializeWriter();
+			writer.append(this.aChar);
+			writer.writeInt(this.anInt);
+			Assert.assertEquals(this.first_out, writer.toString());
+			writer.writeLong(this.aLong);
+			Assert.assertEquals(this.second_out, writer.toString());
+		}
 	}
 
-	@Test
-	public void test_0() throws Exception {
-		//SerializeWriter writer = new SerializeWriter();
-		writer.append(this.chars_to_append[0]);
-		writer.writeInt(this.ints_to_append[0]);
-		Assert.assertEquals(this.first_output_strings[0], writer.toString());
-		writer.writeLong(this.longs_to_append[0]);
-		Assert.assertEquals(this.second_output_strings[0], writer.toString());
+	@RunWith(Parameterized.class)
+	public static class Test_1 {
+		private int anInt;
+		private String first_out;
 
+		private SerializeWriter writer;
+
+		@Parameterized.Parameters
+		public static Collection<Object[]> data() {
+			return Arrays.asList(new Object[][] {
+					{ -1 }
+			});
+		}
+
+		public Test_1(int anInt){
+			this.writer = new SerializeWriter();
+			this.configure(anInt);
+		}
+
+		public void configure(int anInt){
+
+			this. anInt = anInt;
+
+			StringBuilder outputs = new StringBuilder();
+			outputs.append(anInt);
+			this.first_out = outputs.toString();
+		}
+
+		@Test
+		public void test_1() throws Exception {
+			//SerializeWriter writer = new SerializeWriter();
+			writer.writeInt(this.anInt);
+			Assert.assertEquals(this.first_out, writer.toString());
+		}
 	}
 
-	@Test
-	public void test_1() throws Exception {
-		//SerializeWriter writer = new SerializeWriter();
-		writer.writeInt(this.ints_to_append[1]);
-		Assert.assertEquals(this.first_output_strings[1], writer.toString());
+	@RunWith(Parameterized.class)
+	public static class Test_4 {
+		private char aChar;
+		private int anInt;
+		private String first_out;
+
+		private SerializeWriter writer;
+
+		@Parameterized.Parameters
+		public static Collection<Object[]> data() {
+			return Arrays.asList(new Object[][] {
+					{ ',', 1 }
+			});
+		}
+
+		public Test_4(char aChar, int anInt){
+			this.writer = new SerializeWriter();
+			this.configure(aChar, anInt);
+		}
+
+		public void configure(char aChar, int anInt){
+			this.aChar = aChar;
+			this. anInt = anInt;
+
+			StringBuilder outputs = new StringBuilder();
+			outputs.append(anInt).append(aChar);
+			this.first_out = outputs.toString();
+		}
+
+		@Test
+		public void test_4() throws Exception {
+			//SerializeWriter writer = new SerializeWriter();
+			writer.writeInt(this.anInt);
+			writer.write(this.aChar);
+			Assert.assertEquals(this.first_out, writer.toString());
+		}
 	}
 
-	@Test
-	public void test_4() throws Exception {
-		//SerializeWriter writer = new SerializeWriter();
-		writer.writeInt(this.ints_to_append[4]);
-		writer.write(this.chars_to_append[4]);
-		Assert.assertEquals(this.first_output_strings[4], writer.toString());
+	@RunWith(Parameterized.class)
+	public static class Test_5 {
+		private long aLong;
+		private String first_out;
+
+		private SerializeWriter writer;
+
+		@Parameterized.Parameters
+		public static Collection<Object[]> data() {
+			return Arrays.asList(new Object[][] {
+					{ -1L }
+			});
+		}
+
+		public Test_5(long aLong){
+			this.writer = new SerializeWriter();
+			this.configure(aLong);
+		}
+
+		public void configure(long aLong){
+			this.aLong = aLong;
+
+			StringBuilder outputs = new StringBuilder();
+			outputs.append(aLong);
+			this.first_out = outputs.toString();
+		}
+
+		@Test
+		public void test_5() throws Exception {
+			//SerializeWriter writer = new SerializeWriter();
+			writer.writeLong(this.aLong);
+			Assert.assertEquals(this.first_out, writer.toString());
+		}
 	}
 
-	@Test
-	public void test_5() throws Exception {
-		//SerializeWriter writer = new SerializeWriter();
-		writer.writeLong(this.longs_to_append[5]);
-		Assert.assertEquals(this.first_output_strings[5], writer.toString());
-	}
+	@RunWith(Parameterized.class)
+	public static class Test_6 {
+		private char aChar;
+		private long aLong;
+		private String first_out;
 
-	@Test
-	public void test_6() throws Exception {
-		//SerializeWriter writer = new SerializeWriter();
-		writer.writeLong(this.longs_to_append[6]);
-		writer.write(this.chars_to_append[6]);
-		Assert.assertEquals(this.first_output_strings[6], writer.toString());
+		private SerializeWriter writer;
+
+		@Parameterized.Parameters
+		public static Collection<Object[]> data() {
+			return Arrays.asList(new Object[][] {
+					{ ',', -1L }
+			});
+		}
+
+		public Test_6(char aChar, long aLong){
+			this.writer = new SerializeWriter();
+			this.configure(aChar, aLong);
+		}
+
+		public void configure(char aChar, long aLong){
+			this.aChar = aChar;
+			this.aLong = aLong;
+
+			StringBuilder outputs = new StringBuilder();
+			outputs.append(aLong);
+			outputs.append(aChar);
+			this.first_out = outputs.toString();
+		}
+
+		@Test
+		public void test_6() throws Exception {
+			//SerializeWriter writer = new SerializeWriter();
+			writer.writeLong(this.aLong);
+			writer.write(this.aChar);
+			Assert.assertEquals(this.first_out, writer.toString());
+		}
 	}
 }
